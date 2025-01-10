c9eed5cc75b6e19f7b9198db7b4006a1ea2ba7c4
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-09 21:06:56 -0600

    Create Funciones7(Arrow Functions).js
    Arrow functions o funciones flechas, introducidas en ECMAScript 6, son una forma más concisa de escribir funciones en JavaScript.
    
    nombre_function = (parametro) => { //Cuerpo de la funcion};

---
+++ Funciones7(Arrow Functions).js
@@ -0,0 +1,13 @@
+/*                            Ejercicio
+Crea una función flecha llamada convertirMayusculas que tome un parámetro
+texto y devuelva el mismo texto en mayúsculas.
+Ejemplos de uso:
+console.log(convertirMayusculas("hola")); // Debería imprimir: HOLA*/
+
+const convertirMayusculas = (texto) => texto.toUpperCase();
+
+
+/* Fin */
+
+console.log(convertirMayusculas("funciones"));
+console.log(convertirMayusculas("JavaScript"));
