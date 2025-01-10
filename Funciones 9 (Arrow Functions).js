362eb2a20acd3206e9211c1a1e6a8b74179f47ec
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-09 21:35:56 -0600

    Create Funciones 9 (Arrow Functions).js

---
+++ Funciones 9 (Arrow Functions).js
@@ -0,0 +1,16 @@
+/*                                  Ejercicio
+Crea una función flecha sin parámetros llamada saludoBot que debe hacer lo siguiente:
+
+Mostrar en consola el mensaje "Bienvenido, humano".
+Devolver el string "Beep boop".
+Ejemplo de uso:
+
+console.log(saludoBot()); // Debería imprimir "Bienvenido, humano" y luego "Beep boop"
+*/
+
+const saludoBot = () =>  {
+    console.log("Bienvenido, humano");
+    return "Beep boop";    
+};
+
+console.log(saludoBot());
