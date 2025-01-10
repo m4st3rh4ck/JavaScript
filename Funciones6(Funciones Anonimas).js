0d2c0b145cb83ba680fd41035a64f4854c9f9d76
Gabriel Hernandez <gabrielemanuel.hdz@gmail.com>
2025-01-09 20:31:47 -0600

    Create Funciones6(Funciones Anonimas).js
    Una función anónima es una función sin nombre. Puede ser asignada a una variable o pasada directamente como argumento. Son frecuentemente utilizadas cuando necesitamos una función de uso único como argumento de otras funciones.

---
+++ Funciones6(Funciones Anonimas).js
@@ -0,0 +1,12 @@
+/*                    Ejercicio
+Crea una función anónima y pásala como argumento a setTimeout.
+La función debe imprimir el mensaje "¡Hola desde el futuro!" 
+después de 1 segundo (1000 milisegundos).*/
+
+setTimeout(function() {
+
+console.log("¡Hola desde el futuro!")
+
+}, 1000)
+
+console.log("Este mensaje aparece primero");
